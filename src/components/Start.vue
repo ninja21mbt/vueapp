de5ac697916448d476486e6735315979a0e4e59a
Nina Voelker <Nina@Ninas-MBP-4.fritz.box>
2020-03-24 14:09:26 +0100

    c

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -91,12 +91,11 @@ export default {
 and (-webkit-min-device-pixel-ratio: 2) 
 and (orientation: portrait) {
     #box1{
-        background-size: unset;
         height: 100vh;
     }
 
     #box1{
-        background-size: unset;
+      background-size: auto;
     }
 }
 
